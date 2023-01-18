b924047b56f11355c03b2f6290d0df13b89b4ebf
Flavia <Flavia@DESKTOP-VJ0H32R>
2023-01-18 12:36:20 -0400

    Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
+import { HomepageComponent } from './homepage/homepage.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-})
+}
+
+)
 export class AppRoutingModule { }
